9a160e21d6923ba8e0a72a21186870ab981dfe89
Marcus Silva <devsilvamarcus@gmail.com>
2023-04-04 23:31:20 -0300

    :bug: Reparo na sombra ao clicar no menu hamburguer. | Reparo na identacao do  HTML para o Header.

--- src/js/modules/menuMobile.js
+++ src/js/modules/menuMobile.js
@@ -2,15 +2,16 @@ export default function menuMobile() {
   const btnMobile = document.getElementById('btn-mobile');
   const applyShadow = document.querySelector('.shadow');
 
-  
+
   btnMobile.addEventListener('click', toggleMenu);
-  if (window.innerWidth >= 400) {
-    applyShadow.style.display = 'none';
-  }
 
   function toggleMenu() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
-    applyShadow.style.display = 'block';
+    if (nav.classList.contains('active')) {
+      applyShadow.style.display = 'block';
+    } else {
+      applyShadow.style.display = 'none';
+    }
   }
 }
